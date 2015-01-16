8abbc4178b8afc2581eea65800dbd68bd034646e
ishidad <ishidad5@gmail.com>
2015-01-16 18:58:35 -0300

    mas agregados

--- topic0example/src/main/java/com/matin/shopping/cart/Cart.java
+++ topic0example/src/main/java/com/matin/shopping/cart/Cart.java
@@ -21,4 +21,52 @@ public class Cart {
 		return items;
 	}
 
+	public String getPaymentForm() {
+		return paymentForm;
+	}
+
+	public void setPaymentForm(String paymentForm) {
+		this.paymentForm = paymentForm;
+	}
+
+	public boolean isOffer() {
+		return isOffer;
+	}
+
+	public void setOffer(boolean isOffer) {
+		this.isOffer = isOffer;
+	}
+
+	public boolean isDiscount() {
+		return isDiscount;
+	}
+
+	public void setDiscount(boolean isDiscount) {
+		this.isDiscount = isDiscount;
+	}
+
+	public float getFinalPrice() {
+		return finalPrice;
+	}
+
+	public void setFinalPrice(float finalPrice) {
+		this.finalPrice = finalPrice;
+	}
+
+	public int getNroTrans() {
+		return nroTrans;
+	}
+
+	public void setNroTrans(int nroTrans) {
+		this.nroTrans = nroTrans;
+	}
+
+	public String getTransDate() {
+		return transDate;
+	}
+
+	public void setTransDate(String transDate) {
+		this.transDate = transDate;
+	}
+
 }
--- topic0example/src/main/java/com/matin/shopping/cart/User.java
+++ topic0example/src/main/java/com/matin/shopping/cart/User.java
@@ -8,12 +8,36 @@ public class User {
 	private final String userId;
 	private final String userPass;
 	private final String userMail;
-	private final String userFullName;
+	private String userFullName;
 	
 	private final List<Payments> transaccions;
 	
-	public User(){
+	public User(String userId, String userPass, String userMail, String userFullName) {
+		this.userId = userId;
+		this.userPass = userPass;
+		this.userMail = userMail;
+		this.userFullName = userFullName;
 		transaccions = new ArrayList<>();
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public String getUserPass() {
+		return userPass;
+	}
+
+	public String getUserMail() {
+		return userMail;
+	}
+
+	public String getUserFullName() {
+		return userFullName;
+	}
+
+	public List<Payments> getTransaccions() {
+		return transaccions;
 	}
 
 }
