92b2b5a180104c3d36b5d4ece185bab3f0f87b67
ishidad <ishidad5@gmail.com>
2015-01-16 18:30:42 -0300

    Mas clases

--- topic0example/src/main/java/com/matin/shopping/cart/Cart.java
+++ topic0example/src/main/java/com/matin/shopping/cart/Cart.java
@@ -6,6 +6,12 @@ import java.util.List;
 public class Cart {
 
 	private final List<Item> items;
+	private String paymentForm;
+	private boolean isOffer;
+	private boolean isDiscount;
+	private float finalPrice;
+	private int nroTrans;
+	private String transDate;
 
 	public Cart() {
 		items = new ArrayList<>();
--- topic0example/src/main/java/com/matin/shopping/cart/User.java
+++ topic0example/src/main/java/com/matin/shopping/cart/User.java
@@ -1,5 +1,19 @@
 package com.matin.shopping.cart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+	
+	private final String userId;
+	private final String userPass;
+	private final String userMail;
+	private final String userFullName;
+	
+	private final List<Payments> transaccions;
+	
+	public User(){
+		transaccions = new ArrayList<>();
+	}
 
 }
