dac6ef88f8d104a8bcf0b5da2a9efff683461396
ishidad <ishidad5@gmail.com>
2015-01-17 14:56:35 -0300

    fin todas las clases

--- topic0example/src/main/java/com/matin/shopping/cart/Offers.java
+++ topic0example/src/main/java/com/matin/shopping/cart/Offers.java
@@ -1,5 +1,39 @@
 package com.matin.shopping.cart;
 
+import java.util.List;
+
 public class Offers {
+
+	private List<Offers> itemsOffer;
+	private int nroOffer;
+	private int offerPrice;
+	
+	public Offers(){
+		
+	}
+	
+	public void setItemsOffer(List<Offers> itemsOffer) {
+		this.itemsOffer = itemsOffer;
+	}
+	
+	public List<Offers> getItemsOffer() {
+		return itemsOffer;
+	}
+
+	public int getNroOffer() {
+		return nroOffer;
+	}
+
+	public void setNroOffer(int nroOffer) {
+		this.nroOffer = nroOffer;
+	}
+
+	public int getOfferPrice() {
+		return offerPrice;
+	}
+
+	public void setOfferPrice(int offerPrice) {
+		this.offerPrice = offerPrice;
+	}
 
 }
