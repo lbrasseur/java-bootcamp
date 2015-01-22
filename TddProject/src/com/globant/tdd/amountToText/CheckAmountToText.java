fd482ceed12414d4a9d08198528cd85a7df88926
Marcos Dalmasso <marcosadriandalmasso@hotmail.com>
2015-01-22 19:22:01 -0300

    Correcciones sugeridas respecto al nombre de los métodos utilizados para los tests.

--- TddProject/src/com/globant/tdd/amountToText/CheckAmountToText.java
+++ TddProject/src/com/globant/tdd/amountToText/CheckAmountToText.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 public class CheckAmountToText {
 
 	@Test
-	public void checkAmountToText() {
+	public void checkNumbersWithDecimalToText() {
 		Checks check = new Checks(2523.04);
 		assertEquals("Check amount: ", "Two thousand five hundred twenty-three and 04/100 dollars",
 				EnglishNumberToWords.convertToText(check.getAmount()));
 	}
 	
 	@Test
-	public void checkAmountToText2() {
-		Checks check = new Checks(92523.24);
-		assertEquals("Check amount: ", "Ninety-two thousand five hundred twenty-three and 24/100 dollars",
+	public void checkBigNumbersToText() {
+		Checks check = new Checks(52192523.24);
+		assertEquals("Check amount: ", "Fifty-two million one hundred ninety-two thousand five hundred twenty-three and 24/100 dollars",
 				EnglishNumberToWords.convertToText(check.getAmount()));
 	}
 }
