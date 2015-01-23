cf0108ccabbf7214457232945c08d76340710355
Marcos Dalmasso <marcosadriandalmasso@hotmail.com>
2015-01-23 00:29:04 -0300

    Envío práctica 3 - Topic 3

---
+++ TddProject/src/com/globant/tdd/recentFileList/Files.java
@@ -0,0 +1,14 @@
+package com.globant.tdd.recentFileList;
+
+public class Files {
+
+	private String name;
+
+	public Files (String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
