8c0b029152fbd6b9c7079f67d3a18abf37478943
Marcos Dalmasso <marcosadriandalmasso@hotmail.com>
2015-01-22 21:39:04 -0300

    Envío práctica 2 - Topic 3

---
+++ TddProject/src/com/globant/tdd/wordWrapping/WrappedWords.java
@@ -0,0 +1,22 @@
+package com.globant.tdd.wordWrapping;
+
+public class WrappedWords {
+	private String str;
+	private int length;
+
+	public WrappedWords(String str, int length){
+		this.str = str;
+		this.length = length;
+		
+	}
+
+	public String getStr() {
+		return str;
+	}
+
+	public int getLength() {
+		return length;
+	}
+	
+
+}
